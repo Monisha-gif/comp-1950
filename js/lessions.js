a0f74402e7c48719397904104c7c9c84eb27e465
Matin Lotfaliei <matinlotfali@gmail.com>
2019-12-11 00:16:31 -0800

    ughhhhh

--- js/lessions.js
+++ js/lessions.js
@@ -5,19 +5,19 @@
 // It is useful when the tag has height=0 but its children have a height
 function sumHeightOfChildren(tag) {
     var h = 0;
-    tag.children().each(function() { h += $(this).outerHeight( true ); });
+    tag.children().each(function () { h += $(this).outerHeight(true); });
     return h;
 }
 
 // It hides all lession contents after the h2
-function hideAll() {    
+function hideAll() {
     $("h2").removeClass("visible");
     $("h2 + div").removeClass("visible");
     $("h2 + div").css("max-height", 0);
 }
 
 // It scrolls the page smoothly to the pixel given
-function scrollToLocation(location) {    
+function scrollToLocation(location) {
     $('html, body').animate({ scrollTop: location }, 1000);
 }
 
@@ -27,10 +27,10 @@ function scrollToTag(tag) {
 
     //if there is an h2 already open before the given tag, 
     //you must calculate its height as it is closing
-    var visibleContent = $(".content.visible");      
-    if(visibleContent.length && visibleContent.offset().top < clickedLocation) {
+    var visibleContent = $(".content.visible");
+    if (visibleContent.length && visibleContent.offset().top < clickedLocation) {
         var visibleHeight = visibleContent.outerHeight(true);
-        var scrollLocation =  clickedLocation - visibleHeight;        
+        var scrollLocation = clickedLocation - visibleHeight;
         scrollToLocation(scrollLocation);
     }
     else
@@ -38,26 +38,26 @@ function scrollToTag(tag) {
 }
 
 // Instructions for the time an h2 or an anchor with #id is clicked
-function headerClicked(tag) {              
+function headerClicked(tag) {
     scrollToTag(tag);
-    hideAll();        
+    hideAll();
     tag.addClass("visible"); //just used for highlighting the open h2
-    tag.next().addClass("visible");  
+    tag.next().addClass("visible");
     tag.next().css("max-height", sumHeightOfChildren(tag.next()));
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("h2 + div").addClass("content"); //I tried not to touch any part of the lesson html contents that I copied from bcitcomp.ca
-    hideAll();    
+    hideAll();
 
-    $("h2").click(function() {
+    $("h2").click(function () {
         headerClicked($(this));
     });
 
     // for the time an anchor with #id is clicked
-    $('a[href^="#"]').click(function(e) {
+    $('a[href^="#"]').click(function (e) {
         var id = $(this).attr('href');  // which tag is requested?
         headerClicked($(id));
         e.preventDefault();
-    });       
+    });
 });
